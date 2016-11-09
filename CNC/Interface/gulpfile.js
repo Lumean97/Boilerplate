dd42f412536bb8624ee854dcfc5daa946ae5d105
Marcel Herd <me@marcelherd.com>
2016-11-09 22:44:01 +0100

    Not sure if I can amend after pushing upstream xd

--- CNC/Interface/gulpfile.js
+++ CNC/Interface/gulpfile.js
@@ -12,32 +12,7 @@ const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 
-const gutil = require('gulp-util');
-const chalk = require('chalk');
-
-function map_error(err) {
-	if (err.fileName) {
-		// regular error
-		gutil.log(chalk.red(err.name)
-			+ ': '
-			+ chalk.yellow(err.fileName.replace(__dirname + '/src/', ''))
-			+ ': '
-			+ 'Line '
-			+ chalk.magenta(err.lineNumber)
-			+ ' & '
-			+ 'Column '
-			+ chalk.magenta(err.columnNumber || err.column)
-			+ ': '
-			+ chalk.blue(err.description));
-	} else {
-		// browserify error..
-		gutil.log(chalk.red(err.name)
-			+ ': '
-			+ chalk.yellow(err.message));
-	}
-}
-
-gulp.task('watchify', function () {
+gulp.task('watch', function () {
 	var args = merge(watchify.args, { debug: true });
 	var bundler = watchify(browserify('./src/main.js', args)).transform(babelify, { presets: ['es2015'] });
 	bundle_js(bundler);
@@ -47,9 +22,29 @@ gulp.task('watchify', function () {
 	});
 });
 
+// Without watchify
+gulp.task('build', function () {
+	var bundler = browserify('./src/main.js', { debug: true }).transform(babelify, { presets: ['es2015'] });
+
+	return bundle_js(bundler);
+});
+
+// Without sourcemaps
+gulp.task('build-production', function () {
+	var bundler = browserify('./src/main.js').transform(babelify, { presets: ['es2015'] });
+
+	return bundler.bundle()
+		.pipe(source('main.js'))
+		.pipe(buffer())
+		.pipe(rename('app.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['build']);
+
 function bundle_js(bundler) {
 	return bundler.bundle()
-		.on('error', map_error)
 		.pipe(source('main.js'))
 		.pipe(buffer())
 		.pipe(gulp.dest('dist'))
@@ -60,23 +55,3 @@ function bundle_js(bundler) {
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('dist'));
 }
-
-// Without watchify
-gulp.task('browserify', function () {
-	var bundler = browserify('./src/main.js', { debug: true }).transform(babelify, {/* options */ });
-
-	return bundle_js(bundler);
-});
-
-// Without sourcemaps
-gulp.task('browserify-production', function () {
-	var bundler = browserify('./src/main.js').transform(babelify, {/* options */ });
-
-	return bundler.bundle()
-		.on('error', map_error)
-		.pipe(source('main.js'))
-		.pipe(buffer())
-		.pipe(rename('app.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
-});
