0e6d874f55d11dc5e87703ff83c9a1a3a90891fe
Marcel Herd <me@marcelherd.com>
2016-11-30 15:23:47 +0100

    FS API

--- CNC/Server/index.js
+++ CNC/Server/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const fs      = require('fs')
+
 const app     = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  fs.readFile('tasks.json', 'utf-8', (err, data) => {
+
+    if (err) throw err
+
+    fs.writeFile('out.json', data, (err) => {
+      if (err) throw err
+    })
+    
+    res.send(data.toString('utf-8'))
+  })
 })
 
 app.listen(3000, function () {
