3074e5cab0eb593eafb443b310d77f71b48d4fbc
Marcel Herd <me@marcelherd.com>
2016-12-28 18:24:20 +0100

    finished tasks interface

--- CNC/Interface/public/javascripts/tasks.js
+++ CNC/Interface/public/javascripts/tasks.js
@@ -25,11 +25,42 @@ function render() {
 			$('<td>').text(item.id),
 			$('<td>').text(item.type),
 			$('<td>').text(item.data.input),
-			$('<td>').text(item.data.output)
+			$('<td>').text(item.data.output),
+			$('<td>').html('<a class="btn btn-sm btn-default disabled">Delete</a>')
 		);
 		$('#table').append($tr);
 	});
 }
+
+$('#create-task').submit((e) => {
+	e.preventDefault();
+
+	let input = $('#task-input').val();
+	let type = $('#task-type').val();
+
+	let payload = JSON.stringify({
+		type: type,
+		data: {
+			input: input
+		}
+	});
+
+	let request = new Request(api, {
+		method: 'POST',
+		mode: 'CORS',
+		body: payload,
+		headers: new Headers({
+			'Content-Type': 'application/json'
+		})
+	});
+
+	fetch(request).then((response) => {
+		if (response.status == 200) {
+			update();
+			render();
+		}
+	});
+});
 
 update();
 
