f6d343cc2867d114a240559cc7be15346f0c0a83
Marcel Herd <me@marcelherd.com>
2016-12-28 16:48:41 +0100

    Basic HTML layouts done

--- CNC/Interface/public/javascripts/status.js
+++ CNC/Interface/public/javascripts/status.js
@@ -1,1 +1,36 @@
-console.log("Loaded status.js");
+const api = '//localhost:8000/api/status';
+
+let statuses = {};
+
+function update() {
+	let request = new Request(api, {
+		method: 'GET',
+		mode: 'CORS',
+		headers: new Headers({
+			'Content-Type': 'application/json'
+		})
+	});
+
+	fetch(request).then(response => response.json()).then((data) => {
+		statuses = data;
+		render();
+	});
+}
+
+function render() {
+	$('tr:has(td)').remove();
+	$.each(statuses, (index, item) => {
+		let $tr = $('<tr>').append(
+			$('<td>').text(item.id),
+			$('<td>').text(item.ip),
+			$('<td>').text(item.task),
+			$('<td>').text(item.workload),
+			$('<td>').html('TODO')
+		);
+		$('#table').append($tr);
+	});
+}
+
+update();
+
+window.setInterval(update, 10000);
--- CNC/Interface/public/javascripts/tasks.js
+++ CNC/Interface/public/javascripts/tasks.js
@@ -1,1 +1,36 @@
-console.log("Loaded tasks.js");
+const api = '//localhost:8000/api/tasks';
+
+let tasks = {};
+
+function update() {
+	let request = new Request(api, {
+		method: 'GET',
+		mode: 'CORS',
+		headers: new Headers({
+			'Content-Type': 'application/json'
+		})
+	});
+
+	fetch(request).then(response => response.json()).then((data) => {
+		tasks = data;
+		render();
+	});
+}
+
+function render() {
+	$('tr:has(td)').remove();
+	$.each(tasks, (index, item) => {
+		console.log(item);
+		let $tr = $('<tr>').append(
+			$('<td>').text(item.id),
+			$('<td>').text(item.type),
+			$('<td>').text(item.data.input),
+			$('<td>').text(item.data.output)
+		);
+		$('#table').append($tr);
+	});
+}
+
+update();
+
+window.setInterval(update, 10000);
